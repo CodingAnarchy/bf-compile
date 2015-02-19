f3c6129498dd8a0cadacfbad2045c0a0f334af3e
CodingAnarchy <mtanous22@gmail.com>
2015-02-18 18:41:38 -0700

    Initial working version of Brainfuck compiler - doesn't do anything with the data yet

---
+++ bf-compile.c
@@ -0,0 +1,120 @@
+/*
+BF-Compile.c
+C program to compile brainfuck to assembly
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUCCESS 0
+
+char *change_ext (const char* fname, char dot, char sep, const char* new) {
+	char *retstr, *lastdot, *lastsep;
+
+	// Error checks and allocate string
+	if (fname == NULL)
+		return NULL;
+	if ((retstr = malloc(strlen(fname) + 4)) == NULL)
+		return NULL;
+
+	strcpy(retstr, fname);
+	lastdot = strrchr(retstr, dot);
+	lastsep = (sep == 0) ? NULL : strrchr(retstr, sep);
+
+	// If it has an extension separator
+	if (lastdot != NULL) {
+		// and it is after the last separator
+		if (lastsep != NULL) {
+			if (lastsep < lastdot) {
+				*lastdot = new;
+			}
+		} else {
+			// Has extension with no path separator
+			*lastdot = new;
+		}
+	}
+
+	return retstr;
+
+}
+
+int compile_bf(const char *filename) {
+
+	unsigned int ptr = 0;
+	FILE *source;
+	FILE *out;
+	char* dest;
+	char* obj;
+	int c;
+	int jmp = 0;
+
+	if ((source = fopen(filename, "rb")) == NULL) {
+		fprintf(stderr, "Could not open file %s", filename);
+		return -1;
+	}
+	
+	dest = change_ext(filename, '.', '/', ".asm");
+
+	if ((out = fopen(dest, "wb")) == NULL) {
+		fprintf(stderr, "Could not create file %s", dest);
+		return -1;
+	}
+
+	obj = change_ext(filename, '.', '/', ".o");
+
+	// Header information in assembly file
+	fputs("; ", out);
+	fputs(dest, out);
+	fputs("\n; compiled from ", out);
+	fputs(filename, out);
+	fputs("; assemble: nasm -f elf -o ", out);
+	fputs(obj, out);
+	fputs(" ", out);
+	fputs(dest, out);
+	fputs("\nlink: ld ", out);
+	fputs(obj, out);
+	fputs(" <cmd>", out);
+	fputs("\n\n", out);
+
+	// Start of program in assembly file
+	fputs("main:\n", out);
+	fputs("\tmov\tbh,0xa0\n", out);
+	fputs("\tmov\tdi,bx\n", out);
+
+	// Read in characters and write out assembly to handle them
+	while ((c = fgetc(source)) != EOF) {
+		switch(c) {
+			case '>': 
+				break;
+			case '<': break;
+			case '+': break;
+			case '-': break;
+			case '.': break;
+			case ',': break;
+			case '[': break;
+			case ']': break;
+			default: break;
+		}
+	}
+
+	free(dest);
+	free(out);
+
+
+}
+
+int main(int argc, const char *argv[])
+{
+	int status;
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s filename\n", argv[0]);
+	}
+
+	status = compile_bf(argv[1]);
+	if (status != SUCCESS) {
+		fprintf(stderr, "Compilation Error!\n");
+	}
+
+	return status;
+}
