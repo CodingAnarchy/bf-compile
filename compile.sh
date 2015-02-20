a36a0a8546a6f1e0a1127150d0112dbceb39a97f
CodingAnarchy <mtanous22@gmail.com>
2015-02-19 20:44:03 -0700

    Update to link shell script commands to only run if previous was successful

--- compile.sh
+++ compile.sh
@@ -1,9 +1,8 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 filename=${1%.*}
 
-
-gcc -o bf-compile bf-compile.c
-./bf-compile ${filename}.bf
-nasm -s -f elf64 -o ${filename}.o ${filename}.asm
+gcc -o bf-compile bf-compile.c && \
+./bf-compile ${filename}.bf && \
+nasm -s -f elf64 -o ${filename}.o ${filename}.asm && \
 gcc -o ${filename} ${filename}.o
